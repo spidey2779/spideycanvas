c9cba0d3eecc52f6a7e12aab785bcdbbcd539236
spidey2779 <gteja2779@gmail.com>
2024-09-30 23:42:50 +0530

    changed

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
-// export const socket = io('https://spideycanvasbackend.onrender.com');
-export const socket = io('http://localhost:4000');
+export const socket = io('https://spideycanvasbackend.onrender.com');
+// export const socket = io('http://localhost:4000');
 
